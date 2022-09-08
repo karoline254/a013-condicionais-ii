51831a6702fb7f22872e1f0fbc26fca09cb18570
karoline254 <karolineexecut@gmail.com>
2022-09-08 12:03:18 -0300

    Exercicio03

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,26 @@
-//código a ser reescrito
+// //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// // Código 01:
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+// Código usando If ternério 01
+const nome = prompt("Qual o seu nome?")
+nome === "José" ? console.log("Oi, Zé") : console.log("Olá, "+nome);
+
+// // Código 02:
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+// Código usando If ternério 02:
+
+const idade = Number(prompt("Qual a sua idade?"))
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista");
