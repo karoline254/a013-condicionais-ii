7c24c367c26e06dd63d50aa5eb9fb8d36d0ebc99
karoline254 <karolineexecut@gmail.com>
2022-09-08 11:48:51 -0300

    Exercicio01

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,24 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número"))
+
+// Utilizando ifs aninhados:
+
+if(num % 2 === 0){
+    if(num % 3 === 0){
+        console.log("Sim, é divisível por 2 e 3")
+    }else{
+        console.log("Não é divisível!")
+    }
+}else{
+    console.log("Não é divisível")
+}
+
+// Utilizando um operador lógico para unir duas operações relacionais
+
+if(num % 2 === 0 && num % 3 === 0){
+    console.log("Sim, é divisível por 2 e 3");
+}else{
+    console.log("Não, não é divisível!");
+}
+
+
 
